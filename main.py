2ca7d9b1c4bbad84005de27676a19c178fe81bfb
baidyprod <dima220920041@gmail.com>
2023-02-11 21:41:19 +0200

    Initial commit

--- main.py
+++ main.py
@@ -10,8 +10,18 @@ if __name__ == '__main__':
     assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
 
 
-def parse_cookie(query: str) -> dict:
-    return {}
+def parse_cookie(cookie: str) -> dict:
+    items = cookie.split(";")
+    cookie_dict = {}
+    for item in items:
+        if '=' not in item and item != '':
+            cookie_dict[item] = ''
+            continue
+        split_item = item.strip().split("=", 1)
+        if len(split_item) == 2:
+            key, value = split_item
+            cookie_dict[key] = value
+    return cookie_dict
 
 
 if __name__ == '__main__':
@@ -19,3 +29,15 @@ if __name__ == '__main__':
     assert parse_cookie('') == {}
     assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
     assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+    assert parse_cookie(';name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+    assert parse_cookie('name=Dima;age=28;;') == {'name': 'Dima', 'age': '28'}
+    assert parse_cookie('name=Dima=User;age=28;secure') == {'name': 'Dima=User', 'age': '28', 'secure': ''}
+    assert parse_cookie('name=Dima=User;age=28;secure=true') == {'name': 'Dima=User', 'age': '28', 'secure': 'true'}
+    assert parse_cookie(';name=Dima=User;age=28;secure=true;') == {'name': 'Dima=User', 'age': '28', 'secure': 'true'}
+    assert parse_cookie('name=Dima;age="28"') == {'name': 'Dima', 'age': '"28"'}
+    assert parse_cookie('name=Dima;age="28";secure=true') == {'name': 'Dima', 'age': '"28"', 'secure': 'true'}
+    assert parse_cookie('name=Dima;age="28;secure=true"') == {'name': 'Dima', 'age': '"28', 'secure': 'true"'}
+    assert parse_cookie('name=Dima=User;age=28;secure=true;reg=Ttrue;') == {'name': 'Dima=User', 'age': '28', 'secure': 'true', 'reg': 'Ttrue'}
+    assert parse_cookie('name=Dima;age=28;secure=;true') == {'name': 'Dima', 'age': '28', 'secure': '', 'true': ''}
+    assert parse_cookie('name;Dima;age;28;') == {'name': '', 'Dima': '', 'age': '', '28': ''}
+    assert parse_cookie('name=Dima=age=28;') == {'name': 'Dima=age=28'}
